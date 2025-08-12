a519f5453c9b45fe62a1a4c425a86508aaf7529d
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-13 01:12:10 +0500

    gg

--- myapp/app/terms/page.jsx
+++ myapp/app/terms/page.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const terms = () => {
   return (
-    <div>
-        <h1 className="text-5xl text-blue-700">Terms and Conditions</h1>
+    <div className="mt-20 ml-15">
+        <h1 className="text-5xl mb-15 text-blue-700">Terms and Conditions</h1>
+        <div className='text-2xl'>
+          <td className='text-gray-700'><strong className='text-black'>Last Updated</strong>: [13-Aug-2025]</td>
+          <p className='text-gray-700 mt-8'>Welcome to [<strong>Drive Chackr</strong>]. These Terms and Conditions govern your use of our website, services, and any reports or documentation we provide. By accessing our website or booking our services, you agree to be bound by these Terms. Please read them carefully before proceeding.</p>
+          <h1 className='mt-8'><strong>1. Services Provided</strong></h1>
+          <p className='mt-5 text-gray-700'>1.1 <strong>Nature of Service</strong></p>
+          <p className='mt-5 text-gray-700'>We provide vehicle inspection services, including visual checks, mechanical assessments, and written reports. Our inspections are based on the condition of the vehicle at the time of examination.</p>
+          <h1 className='mt-8 text-gray-700'>1.2 <strong>No Guarantee of Future Condition</strong></h1>
+          <h5 className='mt-8 font-bold'>2. Bookings and Payments</h5>
+          <h1 className='mt-5 text-gray-700'>2.1 <storng>Booking Confirmation</storng></h1>
+        </div>
     </div>
   )
 }
