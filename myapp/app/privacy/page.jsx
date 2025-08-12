1b836bcdf40a91ac99d54ea9d7c091c13a1b76fd
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-12 22:14:22 +0500

    hh

--- myapp/app/privacy/page.jsx
+++ myapp/app/privacy/page.jsx
@@ -1,8 +1,24 @@
 export default function PrivicyPolicy () {
     return (
-        <div className="mt-20 ml-10 text-4xl text-">
+        <div className="mt-20 ml-15 text-4xl text-">
             <h1>PRIVACY POLICY</h1>
-            <div className=""></div>
+            <div className="">
+                <p className="mt-4 text-lg ">
+                    We at Drive Chackr. and our affiliates (“we”, “our”, or “us”) respect and understand the importance of maintaining your privacy. This policy (“policy”) explains our privacy practices for processing Personal Information related to you when you visit or use our website (“Website”).
+                    <br />
+                    <br />
+                    <br />
+                    The term “Personal Information” refers to information that either identifies an individual or relates to an identifiable individual. For example, personal data is your name, email address, telephone number, and other contact details which you submit to us or that we collect from your mobile device or application.
+                    <br />
+                    <br />
+                    <br />
+                    Certain provisions of this policy apply only if specific territorial data privacy laws apply to our processing activities of Personal Information related to you, as detailed under the relevant sections in this policy.
+                    <br />
+                    <br />
+                    <br />
+                    For further information, please contact us: info@drivecheckr.com
+                </p>
+            </div>
         </div> 
     )
 }
