87444077d8f84d4f4858e4a487198848cce4a6af
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-13 00:35:27 +0500

    hi

---
+++ myapp/app/terms/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const terms = () => {
+  return (
+    <div>
+        <h1 className="text-5xl text-blue-700">Terms and Conditions</h1>
+    </div>
+  )
+}
+
+export default terms
