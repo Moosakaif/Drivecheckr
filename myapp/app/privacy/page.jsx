aff19b6ce06c94180f1f3d48573e099ed53ebfe5
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-12 23:17:08 +0500

    gg

--- myapp/app/privacy/page.jsx
+++ myapp/app/privacy/page.jsx
@@ -1,14 +1,14 @@
 export default function PrivicyPolicy () {
     return (
-        <div className="mt-20 ml-15 text-4xl text-">
-            <h1 className="text-5xl">PRIVACY POLICY</h1>
-            <div className="text--2xl mt-4">
-                <p className="mt-4 text-lg ">
-                    We at Drive Chackr. and our affiliates (“we”, “our”, or “us”) respect and understand the importance of maintaining your privacy. This policy (“policy”) explains our privacy practices for processing Personal Information related to you when you visit or use our website (“Website”).
+        <div className="mt-20 ml-15">
+            <h1 className="text-5xl mb-15 text-blue-700">PRIVACY POLICY</h1>
+            <div className="text-2xl">
+                <p className="text-gray-700 mt-4">
+                    We at Drive Chackr. and our affiliates (“<strong>we</strong>”, “<strong>our</strong>”, or “<strong>us</strong>”) respect and understand the importance of maintaining your privacy. This policy (“<strong>policy</strong>”) explains our privacy practices for processing Personal Information related to you when you visit or use our website (“<strong>Website</strong>”).
                     <br />
                     <br />
                     <br />
-                    The term “Personal Information” refers to information that either identifies an individual or relates to an identifiable individual. For example, personal data is your name, email address, telephone number, and other contact details which you submit to us or that we collect from your mobile device or application.
+                    The term “<strong>Personal Information</strong>” refers to information that either identifies an individual or relates to an identifiable individual. For example, personal data is your name, email address, telephone number, and other contact details which you submit to us or that we collect from your mobile device or application.
                     <br />
                     <br />
                     <br />
@@ -18,6 +18,13 @@ export default function PrivicyPolicy () {
                     <br />
                     For further information, please contact us: <link rel="stylesheet" href="info@drivecheckr.com" />info@drivecheckr.com
                 </p>
+                <br /><br /><br />
+                <h1 className="font-bold">Personal Information You Provide</h1>
+                <div>
+                    <p>We receive and store any information you enter on our Website or give us in any other way. When you contact us via the Website or register for one of our events or webinars (“<strong>Events</strong>”), we receive any information you provide us, for example:
+
+</p>
+                </div>
             </div>
         </div> 
     )
