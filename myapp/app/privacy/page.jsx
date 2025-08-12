269ecab15ff18de2f90dcf1c104b06bef1661914
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-12 21:58:40 +0500

    hh

---
+++ myapp/app/privacy/page.jsx
@@ -0,0 +1,8 @@
+export default function PrivicyPolicy () {
+    return (
+        <div className="mt-20 ml-10 text-4xl text-">
+            <h1>PRIVACY POLICY</h1>
+            <div className=""></div>
+        </div> 
+    )
+}
