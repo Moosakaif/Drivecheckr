7fe6bf2be6a9f2b65bf242bc7b9dfbab8bfcd203
Abdul Haseeb Hamid <scamers487@gmail.com>
2025-08-14 19:27:14 +0500

    gg

--- myapp/app/terms/page.jsx
+++ myapp/app/terms/page.jsx
@@ -11,8 +11,17 @@ const terms = () => {
           <p className='mt-5 text-gray-700'>1.1 <strong>Nature of Service</strong></p>
           <p className='mt-5 text-gray-700'>We provide vehicle inspection services, including visual checks, mechanical assessments, and written reports. Our inspections are based on the condition of the vehicle at the time of examination.</p>
           <h1 className='mt-8 text-gray-700'>1.2 <strong>No Guarantee of Future Condition</strong></h1>
+          <p className='mt-8 text-gray-700'>Our inspection does not guarantee the future performance, reliability, or safety of the vehicle. We cannot be held responsible for issues that arise after the inspection.</p>
           <h5 className='mt-8 font-bold'>2. Bookings and Payments</h5>
-          <h1 className='mt-5 text-gray-700'>2.1 <storng>Booking Confirmation</storng></h1>
+          <h1 className='mt-5 text-gray-700'>2.1 <strong>Booking Confirmation</strong></h1>
+          <p className='mt-8 text-gray-700'>All bookings must be confirmed via our website, email, or phone. Payment terms will be provided at the time of booking.</p>
+          <h1 className='mt-5 text-gray-700'>2.2 <strong>Cancellation Policy</strong></h1>
+          <p className='mt-8 text-gray-700'>If you wish to cancel or reschedule, please notify us at least [X] hours/days in advance. Cancellations made after this period may be subject to a fee.</p>
+          <h1 className='mt-5 text-gray-700'>2.3 <strong>Non-Refundable Fees</strong></h1>
+          <p className='mt-8 text-gray-700'>Inspection fees are generally non-refundable once the inspection has been completed.</p>
+          <h1 className='mt-8'><strong>3. Reports and Liability</strong></h1>
+          <h2 className='mt-5 text-gray-700'>3.1 <strong>Report Accuracy</strong></h2>
+          
         </div>
     </div>
   )
